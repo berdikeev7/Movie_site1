00d946e4b26a1ebedb2f9728383296826debaa62
User <nurbekberdikeev555@gmail.com>
2026-01-20 18:00:56 +0600

    asagrlrl

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -150,6 +150,7 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
